b999e12dbaaa76f158f0d4438e3d5c4af2b03dfc
hrishi0102 <hrishi0102business@gmail.com>
2025-04-15 17:50:39 +0530

    voice agent added

--- Vapi_AI/src/App.jsx
+++ Vapi_AI/src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect } from "react";
+import Vapi from "@vapi-ai/web";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const vapi_key = import.meta.env.VITE_VAPI_API_KEY;
+  const assistant_id = import.meta.env.VITE_ASSISTANT_ID;
+  const vapi = new Vapi(vapi_key);
+  const startCall = () => {
+    vapi.start(assistant_id);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={startCall}>Start Call</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
