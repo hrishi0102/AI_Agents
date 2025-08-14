5f3a5d6c7073f6f1ce766c14982fcf182d40184c
hrishi0102 <hrishi0102business@gmail.com>
2025-08-14 13:24:05 +0530

    bare bone langgraph

--- langgraph/graph.py
+++ langgraph/graph.py
@@ -1,5 +1,5 @@
 from typing_extensions import TypedDict
-from langgraph.graph import StateGraph
+from langgraph.graph import StateGraph, START, END
 
 class State(TypedDict):
     query: str
@@ -14,3 +14,15 @@ def chat_bot(state: State):
 graph_builder = StateGraph(State)
 
 graph_builder.add_node("chat_bot", chat_bot)
+graph_builder.add_edge(START, "chat_bot")
+graph_builder.add_edge("chat_bot", END)
+
+graph = graph_builder.compile()
+
+def main():
+    user = input("> : ")
+    _state = {"query": user, "result": None}
+    graph_result = graph.invoke(_state)
+    print("graph_result:", graph_result)
+
+main()
