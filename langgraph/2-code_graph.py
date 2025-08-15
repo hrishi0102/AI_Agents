a1973c443a3ef4aee911159e8bec7c6443779923
hrishi0102 <hrishi0102business@gmail.com>
2025-08-15 21:31:55 +0530

    simple graph complete

--- langgraph/2-code_graph.py
+++ langgraph/2-code_graph.py
@@ -1,8 +1,10 @@
 from typing_extensions import TypedDict
+from typing import Literal
 from langgraph.graph import StateGraph, START, END
 from openai import OpenAI
 from dotenv import load_dotenv
 from pydantic import BaseModel
+from IPython.display import Image, display
 import os
 load_dotenv()
 
@@ -22,7 +24,6 @@ class ClassifyMessageResponse(BaseModel):
 class State(TypedDict):
     query: str
     result: str | None
-    accuracy: str | None
     is_coding: bool | None
 
 def classify_message(state: State):
@@ -32,9 +33,15 @@ def classify_message(state: State):
     You are a helpful assistant that classifies messages into different categories. 
     Your job is to classify whether the query is a coding question or related to coding or not.
     Return response in specified JSON boolean only
+    {"is_coding": boolean}
+
+    Rules:
+    - Do NOT include code fences
+    - Do NOT add extra text, explanation, or comments
+    - Return only a single JSON object
     The message to classify is: 
     """
-    llm_response = client.beta.chat.completions.create(
+    llm_response = client.beta.chat.completions.parse(
         model="claude-sonnet-4-0",
         response_format=ClassifyMessageResponse,
         messages=[
@@ -42,3 +49,68 @@ def classify_message(state: State):
             {"role": "user", "content": query}
         ],
     )
+
+    is_coding = llm_response.choices[0].message.parsed.is_coding
+    state["is_coding"] = is_coding
+
+    return state
+
+def route_query(state: State) -> Literal["general_query", "coding_query"]:
+    if state["is_coding"]:
+        print("Routing to coding query handler")
+        return "coding_query"
+    else:
+        print("Routing to general query handler")
+        return "general_query"
+
+def general_query(state: State):
+    query = state["query"]
+    print("Using Gemini to process general query")
+    llm_response = gemini_client.chat.completions.create(
+        model="gemini-2.5-flash",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": query}
+        ],
+    )
+    result = llm_response.choices[0].message.content
+    state["result"] = result
+    return state
+
+def coding_query(state: State):
+    query = state["query"]
+    print("Using Claude to process coding query")
+    llm_response = client.chat.completions.create(
+        model="claude-sonnet-4-20250514",
+        messages=[
+            {"role": "system", "content": "You are a helpful coding assistant."},
+            {"role": "user", "content": query}
+        ],
+    )
+    result = llm_response.choices[0].message.content
+    state["result"] = result
+    return state
+
+graph_builder = StateGraph(State)
+graph_builder.add_node("classify_message", classify_message)
+graph_builder.add_node("route_query", route_query)
+graph_builder.add_node("general_query", general_query)
+graph_builder.add_node("coding_query", coding_query)
+graph_builder.add_edge(START, "classify_message")
+graph_builder.add_conditional_edges("classify_message", route_query)
+graph_builder.add_edge("general_query", END)
+graph_builder.add_edge("coding_query", END)
+graph = graph_builder.compile()
+
+def main():
+    # display(Image(graph.get_graph().draw_mermaid_png()))
+    query = input("> : ")
+    _state = {
+        "query": query,
+        "result": None,
+        "is_coding": None
+    }
+    graph_result = graph.invoke(_state)
+    print("graph_result:", graph_result)
+
+main()
