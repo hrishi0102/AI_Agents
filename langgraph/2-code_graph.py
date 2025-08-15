52fd9305fefbafa998c3fd313cd7e779010e903f
hrishi0102 <hrishi0102business@gmail.com>
2025-08-15 20:31:25 +0530

    pydantic structured output

--- langgraph/2-code_graph.py
+++ langgraph/2-code_graph.py
@@ -2,6 +2,7 @@ from typing_extensions import TypedDict
 from langgraph.graph import StateGraph, START, END
 from openai import OpenAI
 from dotenv import load_dotenv
+from pydantic import BaseModel
 import os
 load_dotenv()
 
@@ -14,6 +15,9 @@ gemini_client = OpenAI(
     api_key=os.getenv("GEMINI_API_KEY"),
     base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
 )
+
+class ClassifyMessageResponse(BaseModel):
+    is_coding: bool
 
 class State(TypedDict):
     query: str
@@ -28,11 +32,13 @@ def classify_message(state: State):
     You are a helpful assistant that classifies messages into different categories. 
     Your job is to classify whether the query is a coding question or related to coding or not.
     Return response in specified JSON boolean only
-    The message to classify is: {query}
+    The message to classify is: 
     """
-    llm_response = client.chat.completions.create(
+    llm_response = client.beta.chat.completions.create(
         model="claude-sonnet-4-0",
+        response_format=ClassifyMessageResponse,
         messages=[
-            {"role": "system", "content": SYSTEM_PROMPT.format(query=query)}
+            {"role": "system", "content": SYSTEM_PROMPT},
+            {"role": "user", "content": query}
         ],
     )
