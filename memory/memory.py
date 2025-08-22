8be4c2587752a2712ad62df9a812932d0559428f
hrishi0102 <hrishi0102business@gmail.com>
2025-08-22 14:50:59 +0530

    qdrant db using docker

--- memory/memory.py
+++ memory/memory.py
@@ -1,11 +1,33 @@
 from mem0 import Memory
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY")
 
 config = {
     "version": "v1.1",
     "embedder": {
         "provider": "gemini",
         "config": {
+            "api-key": GOOGLE_API_KEY,
             "model": "models/text-embedding-004",
+        }
+    },
+    "llm": {
+        "provider": "anthropic",
+        "config": {
+            "api-key": ANTHROPIC_API_KEY,
+            "model": "claude-sonnet-4-20250514",
+        }
+    },
+    "vector_store": {
+        "provider": "qdrant",
+        "config": {
+            "host": "localhost",
+            "port": 6333
         }
     }
 }
