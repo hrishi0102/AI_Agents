50621ad2e0ae304e0e37a46b1974d8e8e6d9216e
hrishi0102 <hrishi0102business@gmail.com>
2025-08-22 15:22:28 +0530

    mem0

--- memory/memory.py
+++ memory/memory.py
@@ -1,8 +1,16 @@
 from mem0 import Memory
 from dotenv import load_dotenv
 import os
+from openai import OpenAI
+from google import genai
 
 load_dotenv()
+
+client = OpenAI(
+    api_key=os.getenv("ANTHROPIC_API_KEY"),
+    base_url="https://api.anthropic.com/v1/"
+)
+
 
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY")
@@ -12,14 +20,14 @@ config = {
     "embedder": {
         "provider": "gemini",
         "config": {
-            "api-key": GOOGLE_API_KEY,
+            "api_key": GOOGLE_API_KEY,
             "model": "models/text-embedding-004",
         }
     },
     "llm": {
         "provider": "anthropic",
         "config": {
-            "api-key": ANTHROPIC_API_KEY,
+            "api_key": ANTHROPIC_API_KEY,
             "model": "claude-sonnet-4-20250514",
         }
     },
@@ -33,3 +41,16 @@ config = {
 }
 
 mem_client = Memory.from_config(config)
+
+def chat():
+    while True:
+        user_input = input("You: ")
+        response = client.chat.completions.create(
+            model="claude-sonnet-4-20250514",
+            messages=[
+                {"role": "user", "content": user_input}
+            ]
+        )
+        print("AI: ", response.choices[0].message.content)
+
+chat()
