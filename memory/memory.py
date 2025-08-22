ad1aba48e900dac522f5d5a75ab42881e7f4586b
hrishi0102 <hrishi0102business@gmail.com>
2025-08-22 15:59:34 +0530

    add and search memories using mem0

--- memory/memory.py
+++ memory/memory.py
@@ -1,6 +1,7 @@
 from mem0 import Memory
 from dotenv import load_dotenv
 import os
+import json
 from openai import OpenAI
 from google import genai
 
@@ -15,13 +16,14 @@ client = OpenAI(
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY")
 
+# Mem0 opensource config
 config = {
     "version": "v1.1",
     "embedder": {
         "provider": "gemini",
         "config": {
             "api_key": GOOGLE_API_KEY,
-            "model": "models/text-embedding-004",
+            "model": "models/text-embedding-004"
         }
     },
     "llm": {
@@ -34,23 +36,42 @@ config = {
     "vector_store": {
         "provider": "qdrant",
         "config": {
+            "collection_name": "mem0",
             "host": "localhost",
-            "port": 6333
+            "port": 6333,
+            "embedding_model_dims": 768
         }
     }
 }
 
+# Initialize Mem0 client
 mem_client = Memory.from_config(config)
 
 def chat():
     while True:
         user_input = input("You: ")
+
+        # Get relevant memories
+        relevant_memories = mem_client.search(query=user_input, user_id="hrishikesh")
+        # Format memories for context
+        memories = [f"ID: {mem.get('id')} Memory: {mem.get('memory')}" for mem in relevant_memories.get("results")]
+
+        SYSTEM_PROMPT = f"You are a MEMORY AWARE helpful AI assistant. Here are some relevant memories for a specific user: {json.dumps(memories)}"
+
         response = client.chat.completions.create(
             model="claude-sonnet-4-20250514",
             messages=[
+                {"role": "system", "content": SYSTEM_PROMPT},
                 {"role": "user", "content": user_input}
             ]
         )
         print("AI: ", response.choices[0].message.content)
+
+        # Add conversation to memory using Mem0
+        mem_client.add([
+            {"role": "user", "content": user_input},
+            {"role": "assistant", "content": response.choices[0].message.content}
+        ],
+        user_id="hrishikesh")
 
 chat()
